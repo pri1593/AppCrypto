8ae33af1b649ecd7ec930ffc60ef57ecc25f4f32
priscila <nahirpricorrea93@gmail.com>
2022-08-22 16:13:11 -0300

    corecccionjs

--- varios.js/main.js
+++ varios.js/main.js
@@ -1,11 +1,40 @@
 const tbody = document.querySelector('tbody');
-const prevBTN = document.querySelector('.left');
-const nextBTN = document.querySelector('.right');
+const prevBTN = document.getElementById('left');
+const nextBTN = document.getElementById('right');
 const pageNumber = document.querySelector('.page-number');
 const form = document.getElementById('form');
 const searchInput = document.querySelector('.input-search');
-const top100 = document.querySelector('top-100');
+const top100 = document.querySelector('.top-btn');
 const btnContainer = document.querySelector('.pagination');
+
+// Formatear numero a usd
+const fomatNum = (num) =>{
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.');
+};
+
+// Formatear ,mil, millones etc
+const nFormater = (numero, digito) =>{
+    const symbol = [
+        { value: 1, symbol: ''},
+        { value: 1e3, symbol: 'k'},
+        { value: 1e6, symbol: 'M'},
+        { value: 1e9, symbol: 'MM'},
+    ];
+    const rx = /\.0+$|(\.[0-9]*[1-9])0+$/;
+    let item = symbol
+    .slice()
+    .reverse()
+    .find(item => {
+        return numero >= item.value;
+    });
+
+    return item
+    ? (numero / item.value).toFixed(digito).replace(rx, '$1') + 
+    item.symbol
+    : '0';
+};
+
+
 
 // Render html
 const renderCoin = (coin) => {
@@ -16,16 +45,16 @@ const {
     name,
     symbol,
     price_usd,
-    volumen24,
+    volume24,
     percent_change_24h,
     percent_change_7d,
 } = coin;
 return ` <tr>
 <td>#${rank}</td>
 <td class="coin-title">${name} (${symbol.toUpperCase()})</td>
-<td>$${price_usd}</td>
-<td>$${market_cap_usd}</td>
-<td>$${volumen24}</td>
+<td>$${fomatNum(price_usd)}</td>
+<td>$${nFormater(market_cap_usd)}</td>
+<td>$${nFormater(volume24)}</td>
 <td><span class="${percent_change_24h < 0 ? 'down' : 'up'}">${percent_change_24h}</span></td>
 <td><span class="${percent_change_7d < 0 ? 'down' : 'up'}">${percent_change_7d}</span></td>
 </tr>
@@ -44,14 +73,35 @@ const renderCoins = (coins, current) =>{
 
     btnContainer.classList.remove('hidden');
     tbody.innerHTML = coins[current].map(renderCoin).join('');
-}
+};
+
+// Desabilita el boton prev
+const disablePreviousBTN = prev =>{
+    if(prev === null){
+        prevBTN.classList.add('disabled')
+    } else {
+        prevBTN.classList.remove('disabled')
+    }
+
+};
+
+// Desabilitar boton next
+const disableNextBTN = (next, total) =>{
+    if(next === total){
+        nextBTN.classList.add('disabled');
+    } else {
+        nextBTN.classList.remove('disabled');
+    }
+
+};
+
 const resetCount = coins =>{
     prev = coins.prev;
     current = coins.current;
     next = coins.next;
     total = coins.total;
     results = coins.results;
-}
+};
 
 
 const loadCoins = async e =>{
@@ -64,17 +114,49 @@ const loadCoins = async e =>{
         top100.classList.remove('hidden');
         form.reset();
     } else {
-        // top100.classList.add('hidden');
+        top100.classList.add('hidden');
     };
-    // pageNumber.innerHTML = current + 1;
+    pageNumber.innerHTML = current + 1;
     renderCoins(results, current);
+    disablePreviousBTN(prev);
+    disableNextBTN(next, total);
+    // Escuchador para next
+    nextBTN.addEventListener('click' , e =>{
+        e.stopImmediatePropagation();
+        if(next === total) return;
+        prev = current;
+        current = next;
+        next += 1;
+        renderCoins(results, current);
+        pageNumber.innerHTML = current + 1;
+        disablePreviousBTN(prev);
+        disableNextBTN(next, total);
 
+    });
+
+    // Escuchador para prev
+    prevBTN.addEventListener('click', e =>{
+        e.stopImmediatePropagation();
+        if(prev === null) return;
+        current -= 1;
+        prev = prev === 0 ? null : prev - 1;
+        next -= 1;
+        renderCoins(results, current);
+        pageNumber.innerHTML = current + 1;
+        disablePreviousBTN(prev);
+        disableNextBTN(next, total);
+        
+    });
 };
-
+//  const showTop100 = () =>{
+//     loadCoins();
+//  };
 
 const init = () => {
     window.addEventListener('DOMContentLoaded', loadCoins);
     form.addEventListener('submit', loadCoins);
+    top100.addEventListener('click', loadCoins)
+
 };
 
 init()
